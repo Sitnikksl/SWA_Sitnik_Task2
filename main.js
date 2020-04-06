d302e72408a66316f9914706b60d2d47309c5e3d
Alexandr Sitnik <sitnik.ksl@gmail.com>
2020-04-06 21:19:29 +0700

    сделал fields

--- main.js
+++ main.js
@@ -19,12 +19,25 @@ function parse(obj) {
     $('.hero').append(temp); // вставляем temp в секцию .hero
 
     for(let key in data.fields){ //перебираем data.fields
-        if(typeof(data.fields[key]) == 'object'){ // если внутри еще есть объекты, то  
+        if(typeof(data.fields[key]) == 'object'){ // если внутри еще есть объекты, то
+            let value = data.fields[key]; // создаем переменную, в которой запоминаем текущее значение
             let tempTag = Object.keys(data.fields[key]); // создаем переменную, в которой будут храниться ключи без значений
             for (let j in tempTag){ // проходимся по этой переменной
                 let tempTagName = tempTag[j]; // во временную переменную присваеваем один элемент массива, который должен являться названием тэга
                 let createTag = document.createElement(tempTagName); // создаем элемент 
                 $('.main-form').append(createTag); // вставляем новый элемент в <form>
+                if(tempTagName == 'label'){
+                    let prop = value.label; // хранится значение текущего ключа
+                    createTag.innerHTML = prop; // вставляем значенеие в HTML разметку
+                }else{
+                    console.log('Сейчас хранится: ', value.input); // просто чекаем чтобы не забыть
+                    let temp = value.input; // записали во временную переменню объект input
+                    for (z in temp){
+                        // console.log(temp[z]);
+                        // console.log(z);
+                        createTag.setAttribute(z, temp[z]);
+                    }
+                }
             }
         }
     }
