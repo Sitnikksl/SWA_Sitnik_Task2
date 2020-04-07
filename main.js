5e9b6e64cd4eb14746a3aaf5b858fd58adabb22b
Alexandr Sitnik <sitnik.ksl@gmail.com>
2020-04-07 19:17:05 +0700

    готовый signin

--- main.js
+++ main.js
@@ -18,28 +18,66 @@ function parse(obj) {
     temp.className = 'main-form';// установили класс для формы
     $('.hero').append(temp); // вставляем temp в секцию .hero
 
-    for(let key in data.fields){ //перебираем data.fields
-        if(typeof(data.fields[key]) == 'object'){ // если внутри еще есть объекты, то
-            let value = data.fields[key]; // создаем переменную, в которой запоминаем текущее значение
-            let tempTag = Object.keys(data.fields[key]); // создаем переменную, в которой будут храниться ключи без значений
-            for (let j in tempTag){ // проходимся по этой переменной
-                let tempTagName = tempTag[j]; // во временную переменную присваеваем один элемент массива, который должен являться названием тэга
-                let createTag = document.createElement(tempTagName); // создаем элемент 
-                $('.main-form').append(createTag); // вставляем новый элемент в <form>
-                if(tempTagName == 'label'){
-                    let prop = value.label; // хранится значение текущего ключа
-                    createTag.innerHTML = prop; // вставляем значенеие в HTML разметку
-                }else{
-                    console.log('Сейчас хранится: ', value.input); // просто чекаем чтобы не забыть
-                    let temp = value.input; // записали во временную переменню объект input
-                    for (z in temp){
-                        // console.log(temp[z]);
-                        // console.log(z);
-                        createTag.setAttribute(z, temp[z]);
+    let parseDataFields = function(){
+        for(let key in data.fields){ //перебираем data.fields
+            if(typeof(data.fields[key]) == 'object'){ // если внутри еще есть объекты, то
+                let value = data.fields[key]; // создаем переменную, в которой запоминаем текущее значение
+                let tempTag = Object.keys(data.fields[key]); // создаем переменную, в которой будут храниться ключи без значений
+                for (let j in tempTag){ // проходимся по этой переменной
+                    let tempTagName = tempTag[j]; // во временную переменную присваеваем один элемент массива, который должен являться названием тэга
+                    let createTag = document.createElement(tempTagName); // создаем элемент 
+                    $('.main-form').append(createTag); // вставляем новый элемент в <form>
+                    if(tempTagName == 'label'){
+                        let prop = value.label; // хранится значение текущего ключа
+                        createTag.innerHTML = prop; // вставляем значенеие в HTML разметку
+                    }else{
+                        let temp = value.input; // записали во временную переменню объект input
+                        for (z in temp){
+                            createTag.setAttribute(z, temp[z]);
+                        }
                     }
                 }
             }
         }
     }
+    parseDataFields();
+
+    let parseDataReferences = function(){
+        for(let key in data.references){ // перебираем references
+            if(typeof(data.references[key]) == 'object'){
+                console.log('Что сейчас хранится в референсес: ',data.references[key]); // просто чек
+                let value = data.references[key]; // создаем переменную в которой хранится значение
+                for (let j in value){ // перебираем 
+                    let tempValue = value[j];
+                    let createRef = document.createElement('a');
+                    if(j == 'text'){ // если поле со значением ТЕКСТ, то создаем ссылку
+                        $('.main-form').append(createRef);
+                        createRef.className = "main-form__link";
+                        createRef.innerHTML = tempValue;
+                    }else{ // если там 'ref', то добавляем атриьбут
+                        let container = document.getElementsByClassName('main-form__link');
+                        container[key].href = tempValue;
+                    }
+                }
+            }
+        }
+    }
+    parseDataReferences();
+
+    let parseDataButtons = function(){
+        for(let key in data.buttons){ // перебираем buttons
+            if(typeof(data.buttons[key]) == 'object'){ 
+                let value = data.buttons[key];
+                for (let j in value){
+                    let tempKey = value[j], //создаем переменную, в которой хранится значение
+                        createTag = document.createElement('button'); // создаем кнопку
+                    $('.main-form').append(createTag); // вставляем эту кнопку
+                    createTag.className = 'btn'; // даем ей класс
+                    createTag.innerHTML = tempKey; //вставляем текст, который ранее сохранили в переменную
+                }
+            }
+        }
+    }
+    parseDataButtons();
 }
 
